150e34e7f79522d50c3e0a6c883691437c072a22
denisotree <denisotree@gmail.com>
2026-08-12 16:11:53 +0100

    chore(release): checksums for v0.8.2

--- packaging/homebrew/tuitab.rb
+++ packaging/homebrew/tuitab.rb
@@ -20,24 +20,24 @@ class Tuitab < Formula
   on_macos do
     on_arm do
       url "https://github.com/denisotree/tuitab/releases/download/v0.8.2/tuitab-v0.8.2-aarch64-apple-darwin.tar.gz"
-      sha256 "FILL_IN_AFTER_RELEASE"
+      sha256 "9048746548ffeabc5d4ba750ec70c6d93b319f1dec45a1743165d63123f268ba"
       version "0.8.2"
     end
     on_intel do
       url "https://github.com/denisotree/tuitab/releases/download/v0.8.2/tuitab-v0.8.2-x86_64-apple-darwin.tar.gz"
-      sha256 "FILL_IN_AFTER_RELEASE"
+      sha256 "baf06593c62d0fc38fb98d8205d7f3b866ac25802cb6fb7c662b858b1b38e838"
       version "0.8.2"
     end
   end
   on_linux do
     on_arm do
       url "https://github.com/denisotree/tuitab/releases/download/v0.8.2/tuitab-v0.8.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "FILL_IN_AFTER_RELEASE"
+      sha256 "097f5bb726fb106f8a6a2dab450d73bebe03c6511041f3cb3f165f261d90fd32"
       version "0.8.2"
     end
     on_intel do
       url "https://github.com/denisotree/tuitab/releases/download/v0.8.2/tuitab-v0.8.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "FILL_IN_AFTER_RELEASE"
+      sha256 "e34e8ff2d07895d40e5a76d92b1ed24ab7eecb58f4289a02df57472f7955d546"
       version "0.8.2"
     end
   end
